9ef5d38d3dba051dda546828e2ed950e94a22176
Glitch (light-dramatic-fragrance) <none>
2022-09-08 17:11:13 +0000

    👧🎊 Checkpoint ./package.json:6543255/28 ./src/components/router.jsx:6543255/81 ./src/App.jsx:6543255/160

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
+// import React from 'react'
 
-import NyTimes from '../pages/NyTimes'
-import NyTimesMovieReviews from '../pages/NyTimesMovieReviews'
-import NyTimesBookReviews from "../pages/NyTimesBookReviews";
-import NyTimesFormula1 from "../pages/NyTimesFormula1";
-import NewYorkerRss from "../pages/NewYorkerRss";
-import WiredScienceRss from "../pages/WiredScienceRss";
-
-
-const routes = {
-  '/': () => <NyTimes />,
-  '/NyTimesMovieReviews': () =>  <NyTimesMovieReviews />,
-  '/NyTimesBookReviews': () =>  <NyTimesBookReviews />,
-  '/NyTimesFormula1': () =>  <NyTimesFormula1 />,
-  '/NewYorkerRss': () =>  <NewYorkerRss />,
-  '/WiredScienceRss': () =>  <WiredScienceRss />,
-}
-
-export default routes
-
-
-
-// import React, { Switch, Route } from 'wouter'
 // import NyTimes from '../pages/NyTimes'
 // import NyTimesMovieReviews from '../pages/NyTimesMovieReviews'
-// import NyTimesBookReviews from '../pages/NyTimesBookReviews'
+// import NyTimesBookReviews from "../pages/NyTimesBookReviews";
+// import NyTimesFormula1 from "../pages/NyTimesFormula1";
+// import NewYorkerRss from "../pages/NewYorkerRss";
+// import WiredScienceRss from "../pages/WiredScienceRss";
 
-// export default () => (
-//   <Switch>
-//     <Route path="/" component={NyTimes} />
-//     <Route path="/nytimesmoviereviews/" component={NyTimesMovieReviews} />
-//     <Route path="/nytimesbookreviews/" component={NyTimesBookReviews} />
-//   </Switch>
-// )
+
+// const routes = {
+//   '/': () => <NyTimes />,
+//   '/NyTimesMovieReviews': () =>  <NyTimesMovieReviews />,
+//   '/NyTimesBookReviews': () =>  <NyTimesBookReviews />,
+//   '/NyTimesFormula1': () =>  <NyTimesFormula1 />,
+//   '/NewYorkerRss': () =>  <NewYorkerRss />,
+//   '/WiredScienceRss': () =>  <WiredScienceRss />,
+// }
+
+// export default routes
+
+
+
+import React, { Switch, Route } from 'wouter'
+import NyTimes from '../pages/NyTimes'
+import NyTimesMovieReviews from '../pages/NyTimesMovieReviews'
+import NyTimesBookReviews from '../pages/NyTimesBookReviews'
+
+export default () => (
+  <Switch>
+    <Route path="/" component={NyTimes} />
+    <Route path="/nytimesmoviereviews/" component={NyTimesMovieReviews} />
+    <Route path="/nytimesbookreviews/" component={NyTimesBookReviews} />
+  </Switch>
+)
