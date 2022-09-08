ea7c073bf6fe8062c67c7e9291c98faebc332d4e
Glitch (light-dramatic-fragrance) <none>
2022-09-08 16:23:19 +0000

    🏹👡 Checkpoint ./src/components/router.jsx:6543255/1200

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,12 +1,29 @@
-import React, { Switch, Route } from 'wouter'
 import NyTimes from '../pages/NyTimes'
 import NyTimesMovieReviews from '../pages/NyTimesMovieReviews'
-import NyTimesBookReviews from '../pages/NyTimesBookReviews'
+import NyTimesBookReviews from "./pages/NyTimesBookReviews";
+import NyTimesFormula1 from "./pages/NyTimesFormula1";
+import NewYorkerRss from "./pages/NewYorkerRss";
+import WiredScienceRss from "./pages/WiredScienceRss";
 
-export default () => (
-  <Switch>
-    <Route path="/" component={NyTimes} />
-    <Route path="/nytimesmoviereviews/" component={NyTimesMovieReviews} />
-    <Route path="/nytimesbookreviews/" component={NyTimesBookReviews} />
-  </Switch>
-)
+
+const routes = {
+  '/': () => <NyTimes />,
+  '/nytimes'
+}
+
+export default routes
+
+
+
+// import React, { Switch, Route } from 'wouter'
+// import NyTimes from '../pages/NyTimes'
+// import NyTimesMovieReviews from '../pages/NyTimesMovieReviews'
+// import NyTimesBookReviews from '../pages/NyTimesBookReviews'
+
+// export default () => (
+//   <Switch>
+//     <Route path="/" component={NyTimes} />
+//     <Route path="/nytimesmoviereviews/" component={NyTimesMovieReviews} />
+//     <Route path="/nytimesbookreviews/" component={NyTimesBookReviews} />
+//   </Switch>
+// )
