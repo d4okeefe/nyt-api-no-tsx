27f59e0eadf23fec2603b853f29e73e820d1ad93
Glitch (light-dramatic-fragrance) <none>
2022-09-08 19:10:39 +0000

    🏝🐗 Checkpoint ./src/pages/NewYorkerRss.jsx:6543255/4603

--- src/pages/NewYorkerRss.jsx
+++ src/pages/NewYorkerRss.jsx
@@ -1,40 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Table from 'react-bootstrap/Table'
-import { format } from 'date-fns'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Table from "react-bootstrap/Table";
+import { format } from "date-fns";
 
 const parseDate = function (d) {
-  let date = new Date(d)
-  return format(date, 'd MMM yyyy')
-}
+  let date = new Date(d);
+  return format(date, "d MMM yyyy");
+};
 
 export default function NewYorkerRss() {
   //   const [rssFeed, setRssFeed] = useState('')
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://www.newyorker.com/feed/everything`, {
-        responseType: 'text',
+        responseType: "text",
       })
       .then((response) => {
-        const xml_string = response.data
+        const xml_string = response.data;
         // setRssFeed(xml_string)
 
-        var parseString = require('xml2js').parseString
+        var parseString = require("xml2js").parseString;
         parseString(xml_string, function (err, result) {
-          const inner_array = []
+          const inner_array = [];
           for (var i = 0; i < 100; i++) {
             // null check first
             if (result.rss.channel[0].item[i]) {
-              inner_array[inner_array.length] = result.rss.channel[0].item[i]
+              inner_array[inner_array.length] = result.rss.channel[0].item[i];
             }
           }
 
-          setData(inner_array)
-        })
-      })
-  }, [])
+          setData(inner_array);
+        });
+      });
+  }, []);
 
   return (
     <div className="NewYorkerTable">
@@ -43,6 +43,7 @@ export default function NewYorkerRss() {
         <thead>
           <tr>
             <th scope="col">Title with Link</th>
+            <th scope="col">Abstract</th>
             <th scope="col">Author</th>
             <th scope="col">Date</th>
           </tr>
@@ -60,12 +61,16 @@ export default function NewYorkerRss() {
                   <div>{r.title}</div>
                 </a>
               </td>
-              <td>{r['dc:creator']}</td>
+              <td>
+                <p>{r.description}</p>
+                <p>{r.category}</p>
+              </td>
+              <td>{r["dc:creator"]}</td>
               <td>{parseDate(r.pubDate)}</td>
             </tr>
           ))}
         </tbody>
       </Table>
     </div>
-  )
+  );
 }
