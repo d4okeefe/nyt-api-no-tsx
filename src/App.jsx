682882823bda654231c8880f95e73d152603cae1
Glitch (light-dramatic-fragrance) <none>
2022-09-09 04:04:36 +0000

    🗽👷 Checkpoint ./package.json:6543255/5244 ./public/index.html:6543255/3541 ./src/App.jsx:6543255/484

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "wouter";
-// import PageRouter from "./components/router";
-
-// import { useRoutes } from "hookrouter"
-// import Routes from './components/router'
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
