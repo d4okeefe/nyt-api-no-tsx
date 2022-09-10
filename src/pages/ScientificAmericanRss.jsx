4cd87818cca4f00a766be0e58d14a4497b9f057f
Glitch (light-dramatic-fragrance) <none>
2022-09-10 01:57:14 +0000

    👣🐲 Checkpoint ./src/pages/ScientificAmericanRss.jsx:6543255/12 ./src/App.jsx:6543255/11

--- src/pages/ScientificAmericanRss.jsx
+++ src/pages/ScientificAmericanRss.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Table from "react-bootstrap/Table";
-import https from 'https-proxy-agent'
+import https from 'https'
 
 import { format } from "date-fns";
 
