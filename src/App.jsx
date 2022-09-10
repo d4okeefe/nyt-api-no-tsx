36ab20d71f5e57e8cc846a8104e35d5e20d98905
Glitch (light-dramatic-fragrance) <none>
2022-09-10 15:07:15 +0000

    💆🚄 Checkpoint ./src/App.jsx:6543255/10376 ./src/pages/NyTimesScience.jsx:6543255/1712 ./src/pages/NyTimesSports.jsx:6543255/1711

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, Route, Redirect } from "wouter";
+import { Link, Route } from "wouter";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,7 +15,9 @@ import NyTimesFormula1 from "./pages/NyTimesFormula1";
 import NewYorkerRss from "./pages/NewYorkerRss";
 import WiredScienceRss from "./pages/WiredScienceRss";
 import NyTimesWorld from "./pages/NyTimesWorld";
-import NyTimesOpinion from "./pages/NyTimesOpinion"
+import NyTimesOpinion from "./pages/NyTimesOpinion";
+import NyTimesScience from "./pages/NyTimesScience";
+import NyTimesSports from "./pages/NyTimesSports";
 // import ScientificAmericanRss from "./pages/ScientificAmericanRss";
 import EspnF1 from "./pages/EspnF1";
 import EspnNBA from "./pages/EspnNBA";
@@ -35,7 +37,6 @@ function App() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-
                 <NavDropdown title="NY Times" id="basic-nav-dropdown">
                   <NavDropdown.Item>
                     <Link href="/nytimes">Top stories</Link>
@@ -44,7 +45,13 @@ function App() {
                     <Link href="/nytimes/worldnews">World news</Link>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
+                    <Link href="/nytimes/science">Science</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
                     <Link href="/nytimes/opinion">Opinions</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <Link href="/nytimes/sports">Sports</Link>
                   </NavDropdown.Item>                  
                   <NavDropdown.Divider />
                   <NavDropdown.Item>
@@ -52,7 +59,7 @@ function App() {
                   </NavDropdown.Item>
                   <NavDropdown.Item>
                     <Link href="/nytimes/moviereviews">Movie reviews</Link>
-                  </NavDropdown.Item>                  
+                  </NavDropdown.Item>
                   <NavDropdown.Item>
                     <Link href="/nytimes/formula1">Formula 1</Link>
                   </NavDropdown.Item>
@@ -64,21 +71,21 @@ function App() {
                 <Nav.Link className="nav-link">
                   <Link href="/wired/science">Wired science</Link>
                 </Nav.Link>
-                { /*
+                {/*
                 <Nav.Link className="nav-link">
                   <Link href="/scientificamerican">Scientific American</Link>
                 </Nav.Link>
-                */ }
+                */}
                 <NavDropdown title="Espn" id="basic-nav-dropdown">
                   <NavDropdown.Item className="nav-link">
                     <Link href="/espn/f1">Formula 1</Link>
                   </NavDropdown.Item>
                   <NavDropdown.Item className="nav-link">
                     <Link href="/espn/nba">NBA</Link>
-                  </NavDropdown.Item> 
+                  </NavDropdown.Item>
                   <NavDropdown.Item className="nav-link">
                     <Link href="/espn/tennis">Tennis</Link>
-                  </NavDropdown.Item> 
+                  </NavDropdown.Item>
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
@@ -90,14 +97,16 @@ function App() {
         <Route path="/nytimes" component={NyTimes} />
         <Route path="/nytimes/worldnews" component={NyTimesWorld} />
         <Route path="/nytimes/opinion" component={NyTimesOpinion} />
+        <Route path="/nytimes/sports" component={NyTimesSports} />
+        <Route path="/nytimes/science" component={NyTimesScience} />
         <Route path="/nytimes/moviereviews" component={NyTimesMovieReviews} />
         <Route path="/nytimes/bookreviews" component={NyTimesBookReviews} />
         <Route path="/nytimes/formula1" component={NyTimesFormula1} />
 
-        { /* <Route path="/scientificamerican" component={ScientificAmericanRss} /> */ }
+        {/* <Route path="/scientificamerican" component={ScientificAmericanRss} /> */}
         <Route path="/newyorker" component={NewYorkerRss} />
         <Route path="/wired/science" component={WiredScienceRss} />
-        
+
         <Route path="/espn/f1" component={EspnF1} />
         <Route path="/espn/NBA" component={EspnNBA} />
         <Route path="/espn/tennis" component={EspnTennis} />
---
+++ src/pages/NyTimesSports.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Table from 'react-bootstrap/Table'
+import { format_date } from '../FormatDate'
+
+export default function NyTimesOpinion() {
+  const nyt_api_key = 'O7qlJESoWIOLRSrMh63pU90FoTtWT8Fw'
+  const [data, setData] = useState([])
+
+//  https://api.nytimes.com/svc/topstories/v2/opinion.json?api-key=O7qlJESoWIOLRSrMh63pU90FoTtWT8Fw
+  
+  useEffect(() => {
+    axios
+      .get(`https://api.nytimes.com/svc/topstories/v2/sports.json`, {
+        params: {
+          'api-key': nyt_api_key,
+        },
+      })
+      .then((response) => setData(response.data.results))
+  }, [])
+
+  return (
+    <div className="NyTimesTable">
+      <h4 className="mx-2">N.Y. Times opinions</h4>
+      <Table className="newsDataTable striped bordered hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Title with Link</th>
+            <th scope="col">Abstract</th>
+            <th scope="col">Author</th>
+            <th scope="col">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.filter((r) => {
+            return !(r.title.trim() === '')
+          }).map((r, index) => (
+            <tr key={index}>
+              <td>
+                <a
+                  className="link-info"
+                  href={r.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div>{r.title}</div>
+                </a>
+              </td>
+              <td>{r.abstract}</td>
+              <td>{r.byline}</td>
+              <td>{format_date(r.published_date)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  )
+}
